36b8b7f75189ef6a3ea97995ccb273e14d2a6d66
Anastasiia Lytviak <lav21051991@gmail.com>
2022-05-27 01:27:52 +0300

    add 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+
+const refs = {
+    wrapGallery: document.querySelector(".gallery"),
+}
+
+const markupGalleryItems = createsPictureCards(galleryItems);
+
+refs.wrapGallery.insertAdjacentHTML('beforeend', markupGalleryItems)
+
+function createsPictureCards(items) {
+    return items.map(({ preview, original, description }) => {
+            return `
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image"
+            src="${preview}" 
+            alt="${description}" 
+            title="${description}"/>
+            </a> `
+        })
+        .join('')
+}
+
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+})
+
+let style = document.createElement('STYLE');
+style.type = 'text/css';
+style.innerHTML =
+    '.sl-overlay {background: linear-gradient(160deg, black, transparent)}';
+document.querySelector('body').append(style);
