dfdc021b65df86e505d6f650ceb133013b26578b
Anastasiia Lytviak <lav21051991@gmail.com>
2022-05-30 20:01:13 +0300

    fixed 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -9,7 +9,8 @@ const refs = {
 
 const LOCALSTORAGE_KEY = "feedback-form-state";
 
-const formData = {};
+// const formData = {};
+let formData;
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onFormInput, 500));
@@ -18,26 +19,35 @@ addToText()
 
 function onFormSubmit(e) {
     e.preventDefault();
+
     if (!formData['email'] || !formData['message']) {
         return alert('Not all fields are filled');
     }
-
     e.currentTarget.reset();
 
     localStorage.removeItem(LOCALSTORAGE_KEY);
+
+    for (const key in formData) {
+        delete formData[key];
+    }
+    console.log(formData)
 }
 
 function onFormInput(e) {
     formData[e.target.name] = e.target.value;
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+
+    console.log(formData[e.target.name]);
 }
 
 function addToText() {
-    const savedTextInput = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-    console.log(savedTextInput)
-    if (savedTextInput) {
-        for (const key in savedTextInput) {
-            refs.form[key].value = savedTextInput[key];
+    formData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    console.log(formData)
+    if (formData) {
+        for (const key in formData) {
+            refs.form[key].value = formData[key];
         }
+    } else {
+        formData = {};
     }
 }
